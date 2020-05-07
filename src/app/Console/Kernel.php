16e13a57a8b5dd978c94e587719c81e22155870d
Grant Bartlett <hello@grant-bartlett.com>
2020-05-07 11:46:46 +0100

    Hourly tasks

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('App\Http\Controllers\FeedController@runTask')->everyMinute();
+        $schedule->call('App\Http\Controllers\FeedController@runTask')->hourly();
     }
 
     /**
