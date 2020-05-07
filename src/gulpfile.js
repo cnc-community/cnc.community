df0c9f09be5aa55a24bffddfeb8a121f85baf111
Grant Bartlett <hello@grant-bartlett.com>
2020-05-07 15:35:58 +0100

    Add stylesheets for front end app

---
+++ src/gulpfile.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// Load plugins
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const terser = require('gulp-terser');
+
+
+//
+// Watch SCSS.
+function watchFiles() 
+{
+    gulp.watch(["./resources/stylesheets/**/*.scss"], cssTask);
+}
+
+
+// 
+// Compile SCSS to CSS
+function cssTask()
+{
+    return gulp
+        .src("./resources/stylesheets/app.scss")
+        .pipe(sass({
+            outputStyle: "compressed"
+        }).on("error", sass.logError))
+        .pipe(autoprefixer({
+            browsers: ["last 2 versions"]
+        }))
+        .pipe(gulp.dest("./public/css/"));
+}
+
+
+// 
+// Clears out any things we shouldn't have in js files
+function compileJsTask()
+{
+    return gulp.src([
+        './resources/js/.js',
+    ])
+        .pipe(terser({
+            output: {
+                comments: false,
+            },
+            compress: {
+                drop_console: true,
+            },
+        }))
+        .pipe(gulp.dest('./public/js'));
+}
+
+const defaultTask = gulp.series(
+    cssTask,
+    // compileJsTask
+    watchFiles
+);
+
+const buildTask = gulp.series(
+    cssTask,
+    // compileJsTask
+);
+
+
+// Default task when calling gulp
+exports.default = defaultTask;
+exports.build = buildTask;
