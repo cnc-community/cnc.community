1fa93304dde426fe5ba5ee8dab5498e11d8bfb97
Grant Bartlett <hello@grant-bartlett.com>
2020-05-08 18:17:29 +0100

    Rough game detail page styling

--- src/resources/views/games/detail-how-to-play.blade.php
+++ src/resources/views/games/detail-how-to-play.blade.php
@@ -1,30 +1,63 @@
-@extends('layouts.admin')
+@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Game Detail - {{ $page->title }}</div>
+@section('title', $page->title)
+@section('page-class', 'game-detail')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <h3>How to play</h3>
-                    <?php echo App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_STEPS); ?>
-
-                    <h3>Watch the video</h3>
-                    <?php echo App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_VIDEO); ?>
-
-                    <h3>Discord</h3>
-                    <?php echo App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_HELP); ?>
-                </div>
-            </div>
-        </div>
+@section('hero')
+<div class="content center">
+    <h1 class="text-uppercase">
+        {{ $page->title }}
+    </h1>
+    <p class="lead">
+        {{ $page->description }}
+    </p>
+    <div class="buttons">
+        <a href="#where-to-get" class="btn btn-primary">Where to get</a>
+        <a href="#how-to-play" class="btn btn-primary">How to play</a>
     </div>
 </div>
+@endsection
+
+@section('content')
+<?php $howToPlaySteps = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_STEPS); ?>
+<?php if($howToPlaySteps): ?>
+<section class="section how-to-play-steps">
+    <div class="main-content">
+        <h2 class="section-title">Step by step instructions</h2>
+        <p class="section-description">
+            Consectetur adipiscing elit, sed do eiusmod
+            tempor incidid unt
+        </p>
+        <?php print $howToPlaySteps; ?>
+    </div>
+</section>
+<?php endif; ?>
+
+<?php $howToPlayVideo = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_VIDEO); ?>
+<?php if($howToPlayVideo): ?>
+<section class="section video-tutorial">
+    <div class="main-content">
+        <h2 class="section-title">Watch the video tutorial</h2>
+        <p class="section-description">
+            Consectetur adipiscing elit, sed do eiusmod
+            tempor incidid unt
+        </p>
+        <?php print $howToPlayVideo; ?>
+    </div>
+</section>
+<?php endif; ?>
+
+<?php $howToPlayHelp = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_HELP); ?>
+<?php if($howToPlayHelp): ?>
+<section class="section how-to-play-help">
+    <div class="main-content">
+        <h2 class="section-title">Need help? Ask in these support channels.</h2>
+        <p class="section-description">
+            Consectetur adipiscing elit, sed do eiusmod
+            tempor incidid unt
+        </p>
+        <?php print $howToPlayHelp; ?>
+    </div>
+</section>
+<?php endif; ?>
 @endsection
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - C&C Community</title>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Oswald:wght@300;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/app.css" />
+    <link rel="stylesheet" href="/assets/css/app.css" />
 </head>
 <body>
 
