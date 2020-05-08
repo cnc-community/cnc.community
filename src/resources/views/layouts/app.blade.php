2617ee4f1edeb92e2e2e983e49b79d0ace2e4f97
Grant Bartlett <hello@grant-bartlett.com>
2020-05-08 12:41:17 +0100

    Font size changes for mobile and desktop

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - C&C Community</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Oswald:wght@300;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/app.css" />
 </head>
 <body>
