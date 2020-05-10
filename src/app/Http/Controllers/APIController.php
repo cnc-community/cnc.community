d66a867cb906a8d77668ebad1791606caff5d3f1
Grant Bartlett <hello@grant-bartlett.com>
2020-05-10 16:48:32 +0100

    Add all C&C game titles

--- src/app/Http/Controllers/APIController.php
+++ src/app/Http/Controllers/APIController.php
@@ -15,7 +15,23 @@ class APIController extends Controller
 
     public function streamCount()
     {
-        $data = $this->twitchStreamsAPI->getStreamByGames(["235","10393","14999","1421","4012","1900","20015","16580","5090","3813"]);
+        $data = $this->twitchStreamsAPI->getStreamByGames(
+            [
+                "235", // Command & Conquer: Red Alert
+                "10393", // Command & Conquer: Red Alert - Counterstrike
+                "14999", // Command & Conquer: Red Alert - The Aftermath
+                "4012", // Command & Conquer
+                "1900", // Command & Conquer: Tiberian Sun
+                "20015", // Command & Conquer: Tiberian Sun Firestorm
+                "16580", // Command & Conquer: Red Alert 2
+                "5090", // Command & Conquer: Yuri's Revenge
+                "3813", // Command & Conquer: Renegade
+                "18881", // Command & Conquer: Red Alert 3
+                "18733", // Command & Conquer 3: Kane's Wrath
+                "16106", // Command & Conquer 3: Tiberium Wars
+                "10070", // Command & Conquer: Generals
+                "16487", // Command & Conquer: Zero hour
+            ]);
         $count = $this->twitchStreamsAPI->getCounts($data);
         return $count;
     }
--- src/resources/views/pages/creators/listing.blade.php
+++ src/resources/views/pages/creators/listing.blade.php
@@ -15,10 +15,18 @@
     <div class="twitch-games-navbar">
         <a href="?game_id=4012" id="game-id-4012" class="twitch-game-link">Tiberian Dawn <span class="count">0</span></a>
         <a href="?game_id=235" id="game-id-235" class="twitch-game-link">Red Alert <span class="count">0</span></a>
+        <a href="?game_id=10393" id="game-id-10393" class="twitch-game-link">Red Alert - Counterstrike <span class="count">0</span></a>
+        <a href="?game_id=14999" id="game-id-14999" class="twitch-game-link">Red Alert - The Aftermath <span class="count">0</span></a>
         <a href="?game_id=1900" id="game-id-1900" class="twitch-game-link">Tiberian Sun <span class="count">0</span></a>
+        <a href="?game_id=20015" id="game-id-20015" class="twitch-game-link">Tiberian Sun Firestorm <span class="count">0</span></a>
         <a href="?game_id=16580" id="game-id-16580" class="twitch-game-link">Red Alert 2 <span class="count">0</span></a>
         <a href="?game_id=5090" id="game-id-5090" class="twitch-game-link">Yuri's Revenge <span class="count">0</span></a>
         <a href="?game_id=3813" id="game-id-3813" class="twitch-game-link">Renegade <span class="count">0</span></a>
+        <a href="?game_id=18881" id="game-id-18881" class="twitch-game-link">Red Alert 3 <span class="count">0</span></a>
+        <a href="?game_id=18733" id="game-id-18733" class="twitch-game-link">C&C 3: Kane's Wrath <span class="count">0</span></a>
+        <a href="?game_id=16106" id="game-id-16106" class="twitch-game-link">C&C 3: Tiberium Wars <span class="count">0</span></a>
+        <a href="?game_id=10070" id="game-id-10070" class="twitch-game-link">C&C: Generals <span class="count">0</span></a>
+        <a href="?game_id=16487" id="game-id-16487" class="twitch-game-link">C&C: Zero hour <span class="count">0</span></a>
     </div>
 </div>
 @endsection
