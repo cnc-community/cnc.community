f233f0d2aeb3964e562d4c9596d6a71d6931b798
Grant Bartlett <hello@grant-bartlett.com>
2020-05-23 14:55:54 +0100

    Rough pass at a dropdown in navbar

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -19,13 +19,41 @@
                     </a>
                 </div>
                 <nav>
-                    <div>
-                        <a href="/#games" title="Games">Games</a>
-                        {{-- <a href="#">Communities</a> --}}
-                        <a href="/creators" id="navCreators" title="Creators">Creators <span class="online">0</span></a>
-                        <a href="/remasters" title="Remasters">Remasters</a>
-                        <a href="/funny" title="Funny/Cool">Funny/Cool</a>
-                        {{-- <a href="#">Modding</a> --}}
+                    <div class="navbar-items">
+                        <div class="nav-item nav-item-dropdown">
+                            <a href="/#games" title="Games" class="nav-link">Games <i class="icon icon-dropdown"></i></a>
+
+                            <div class="nav-dropdown-contents">
+                                <div class="dropdown-container">
+                                    <div class="category">
+                                        <div class="title">C&amp;C Games</div>
+                                        <a href="/tiberian-dawn" title="Tiberian Dawn">Tiberian Dawn</a>
+                                        <a href="/red-alert" title="Red Alert">Red Alert</a>
+                                        <a href="/tiberian-sun" title="Tiberian Sun">Tiberian Sun</a>
+                                        <a href="/red-alert-2" title="Red Alert 2">Red Alert 2</a>
+                                        <a href="/renegade" title="Renegade">Renegade</a>
+                                        <a href="/generals" title="Generals">Generals</a>
+                                        <a href="/red-alert-3" title="Renegade">Red Alert 3</a>
+                                        <a href="/command-and-conquer-3" title="C&C 3">C&amp;C 3</a>
+                                        <a href="/command-and-conquer-4" title="C&C 4">C&amp;C 4</a>
+                                    </div>
+                                    <div class="category">
+                                        <div class="title">2020 Games</div>
+                                        <a href="/remasters" title="Remasters Collection">C&amp;C Remaster Collection </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="nav-item">
+                            <a href="/creators" id="navCreators" title="Creators" class="nav-link">Creators <span class="online">0</span></a>
+                        </div>
+                        <div class="nav-item">
+                            <a href="/remasters" title="Remasters" class="nav-link">Remasters</a>
+                        </div>
+                        <div class="nav-item">
+                            <a href="/funny" title="Funny/Cool" class="nav-link">Funny/Cool</a>
+                        </div>
                     </div>
                     <div class="social-links">
                         <a href="https://www.facebook.com/groups/commandandconquer" title="C&C Facebook Group"><i class="icon-facebook"></i></a>
@@ -70,5 +98,36 @@
 
     @yield('scripts')
     <script defer src="/assets/js/SiteCountNav.js"></script>
+    <script>
+        let navitems = document.querySelectorAll(".nav-item");
+        console.log(navitems);
+        for(let i = 0; i < navitems.length; i++)
+        {
+            let navItem = navitems[i];
+            let navItemChildren = navItem.querySelector(".nav-dropdown-contents");
+
+            navItem.addEventListener("mouseenter", function()
+            {   
+                if (navItem.classList.contains("open"))
+                {
+                    console.log(navItem.classList.contains("open"));
+                    return;
+                }
+                navItem.classList.add("open");
+                navItem.classList.remove("close");
+                navItemChildren.style.display = "flex";
+            }, false);  
+
+            if(navItemChildren == null) continue;
+            navItemChildren.addEventListener("mouseenter", function()
+            {
+                navItem.addEventListener("mouseleave", function()
+                {
+                    navItem.classList.remove("open");
+                    navItem.classList.add("close");
+                }, false);
+            }, false);
+        }
+    </script>
 </body>
 </html>
