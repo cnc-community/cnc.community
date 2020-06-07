659946ddba1acc242b8985144a71f6e50400b8a7
Grant Bartlett <hello@grant-bartlett.com>
2020-06-07 19:31:22 +0100

    Move sessions handling to admin routes only

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -31,11 +31,14 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'admin' => [
+            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
         'api' => [
