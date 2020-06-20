c102d862f07df602edeb2a4318121a5df521bd14
Grant Bartlett <hello@grant-bartlett.com>
2020-06-20 10:08:34 +0100

    Turns twitch live count into Live

--- src/resources/typescript/SiteCountNav.ts
+++ src/resources/typescript/SiteCountNav.ts
@@ -4,8 +4,11 @@ export class SiteCountNav
 {
     constructor()
     {
+        /*
         const twitch = new GetTotalStreamCount(this.onComplete.bind(this));
         twitch.get();
+        */
+        this.onComplete(-1); // for now
     }
 
     private onComplete(count): void
@@ -21,6 +24,11 @@ export class SiteCountNav
                 {
                     notificationEl.classList.add("pop-in");
                     notificationEl.innerText = count;
+                }
+                else if (count == -1)
+                {
+                    notificationEl.classList.add("pop-in");
+                    notificationEl.innerText = "Live";
                 }
             }
         }
