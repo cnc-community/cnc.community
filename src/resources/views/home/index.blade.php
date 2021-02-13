b0d8943da43d1e93b7d0588ec8a6830d1d951d40
Grant Bartlett <hello@grant-bartlett.com>
2021-02-13 15:41:08 +0000

    Add event details about Renegade

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
 <div class="content center">
     <h1>Welcome Back Commander</h1>
     <p class="lead">
-        Play C&amp;C on Windows 10 and get all of the <br class="hide-for-xs"/>latest news and content from the community.
+        Play C&amp;C on Windows 10 and get all of the <br class="hide-for-xs" />latest news and content from the community.
     </p>
     <div class="buttons">
         <a class="btn btn-secondary" href="#games" title="Discover C&C">Discover C&amp;C</a>
@@ -20,32 +20,36 @@
 @section('content')
 <section id="games" class="how-to-guides">
     <div class="main-content">
-        <h2 class="section-title">Discover <br class="hide-for-xs"/>Command &amp; Conquer</h2>
+        <h2 class="section-title">Discover <br class="hide-for-xs" />Command &amp; Conquer</h2>
         <p class="section-description">
             Choose a game to find out how to play on Windows 10 and stay up to date with the latest news from around the community.
         </p>
     </div>
-        
+
     <div class="guides">
         <?php new App\Http\CustomView\Components\GameSlider($__env); ?>
     </div>
 </section>
 
-<section id="tiberiandawn" class="section section-grey-alt cnc-remastered-introduction">
+<section id="tiberiandawn" class="section section-grey-alt">
     <div class="main-content center">
         <div class="feature-box">
             <div class="col-50 feature-text">
-                <h1 class="section-title"><span class="light">C&amp;C Remastered </span> <br /> Has Launched!</h1>
+                <h1 class="section-title"><span class="light">Command &amp; Conquer Renegade </span> <br />Turns 19 Years old</h1>
                 <p>
-                    Developed alongside the C&amp;C Community, the Command & Conquer Remastered Collection Delivers 4K Graphics, 
-                    Rebuilt Multiplayer, Enhanced UI, the Completely Remastered Legendary Soundtrack by Frank Klepacki and more.
+                    Join us for a week of games and streams as we celebrate the birthday of one of the hidden gems of the C&C franchise.
+                    Renegade was a first and third person shooter released in 2002 by Westwood Studios which combined tactical action with real time strategy
+                    elements found in other titles in the franchise.
+                    Renegade's multiplayer mode was wholly unique and still captures the hearts and minds of C&C fans who continue to play and mod it to this day.
                 </p>
             </div>
             <div class="col-50 feature-logo">
-                <img src="/assets/images/logos/cnc-remastered-logo.png" alt="Tiberian Dawn Remastered" loading="lazy" />
+                <img src="/assets/images/renegade-19-years-logo.png" alt="Renegade turns 19 years old logo event" loading="lazy" />
 
                 <div class="buttons">
-                    <a href="/command-and-conquer-remastered/" class="btn btn-primary" title="Maps">View C&amp;C Remastered Collection</a>
+                    <a href="https://w3dhub.com/forum/topic/422694-cc-renegades-19th-anniversary-event/" class="btn btn-secondary" title="Read more" target="_blank" rel="nofollow noreferrer">
+                        Find Out More About the Event
+                    </a>
                 </div>
             </div>
         </div>
@@ -56,9 +60,9 @@
     <div class="main-content center">
         <div class="feature-box">
             <div class="col-50 feature-text">
-                <h1 class="section-title"><span class="light">C&amp;C Remastered</span> Leaderboards<br/> have arrived!</h1>
+                <h1 class="section-title"><span class="light">Command &amp; Conquer Remastered</span> Leaderboards</h1>
                 <p class="section-description">
-                    Find the elite in our new C&amp;C Remastered Leaderboards for Tiberian Dawn and Red Alert.
+                    Find the elite in our new Command &amp; Conquer Remastered Leaderboards for Tiberian Dawn and Red Alert.
                 </p>
                 <div class="buttons">
                     <a href="/command-and-conquer-remastered/leaderboard" class="btn btn-secondary" title="View All Leaderboards">View Leaderboards</a>
@@ -92,8 +96,8 @@
             <div class="feature-text">
                 <h1 class="section-title">Our Mission</h1>
                 <p class="section-description">
-                    C&amp;C Community is an ongoing project that aims to improve the accessibility of the Command &amp; Conquer 
-                    the franchise and celebrate the tireless efforts of the wider communities that keep the games alive with mods, 
+                    C&amp;C Community is an ongoing project that aims to improve the accessibility of the Command &amp; Conquer
+                    the franchise and celebrate the tireless efforts of the wider communities that keep the games alive with mods,
                     video content, streams and compatibility patches for moderns operating systems.
                 </p>
                 <div class="buttons">
@@ -112,7 +116,7 @@
     <div class="main-content text-center">
         <h2 class="section-title">Affiliates</h2>
     </div>
-        
+
     <div class="sites">
         <?php new App\Http\CustomView\Components\Sites(); ?>
     </div>
