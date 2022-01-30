2440de69f4902eb302b3b4ab77167a1441a4c00b
OWA <benleech2@gmail.com>
2022-01-30 18:42:34 +0000

    Updated the Links on the "Other C&C Games" Page

--- src/resources/views/games/category-more.blade.php
+++ src/resources/views/games/category-more.blade.php
@@ -29,28 +29,28 @@
 
             <?php new \App\Http\CustomView\Components\GenericBoxItem(
                 $image = "/assets/images/more-cnc-games/ss.jpg",
-                $url = "https://cnc.gamepedia.com/Command_%26_Conquer:_Sole_Survivor",
+                $url = "https://cnc-comm.com/sole-survivor",
                 $title = "Command &amp; Conquer: Sole Survivor",
                 $description = "Sole Survivor was a unique multiplayer-only spin-off of Tiberian Dawn, where players controlled a single unit in a deathmatch-style game mode."
             ); ?>
 
             <?php new \App\Http\CustomView\Components\GenericBoxItem(
                 $image = "/assets/images/more-cnc-games/cnc4.jpg",
-                $url = "https://store.steampowered.com/app/47700/Command__Conquer_4_Tiberian_Twilight/",
+                $url = "https://www.ea.com/games/command-and-conquer/command-and-conquer-4-tiberian-twilight",
                 $title = "Command &amp; Conquer 4: Tiberian Twilight",
                 $description = "Tiberian Twilight is a real-time tactics game and is the final C&C game to be set in the Tiberian Universe."
             ); ?>
 
             <?php new \App\Http\CustomView\Components\GenericBoxItem(
                 $image = "/assets/images/more-cnc-games/ta.jpg",
-                $url = "https://www.tiberiumalliances.com/home",
+                $url = "https://www.ea.com/games/command-and-conquer/command-and-conquer-tiberium-alliances",
                 $title = "Command &amp; Conquer: Tiberium Alliances",
                 $description = "Tiberium Alliances is a free-to-play, massively-multiplayer, online real-time strategy game for web browsers. "
             ); ?>
 
              <?php new \App\Http\CustomView\Components\GenericBoxItem(
                 $image = "/assets/images/more-cnc-games/rivals.jpg",
-                $url = "https://www.ea.com/en-gb/games/command-and-conquer/command-and-conquer-rivals",
+                $url = "https://www.ea.com/games/command-and-conquer/command-and-conquer-rivals",
                 $title = "Command &amp; Conquer: Rivals",
                 $description = "C&C Rivals is a free-to-play, competitive, multiplayer real-time strategy game for Android and IOS."
             ); ?>
