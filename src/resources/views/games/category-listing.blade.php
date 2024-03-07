e73a768c76df3757152f748720dd5d11ad84b9f5
Grant Bartlett <hello@grant-bartlett.com>
2024-03-07 18:41:25 +0000

    Update pages

--- src/resources/views/games/category-listing.blade.php
+++ src/resources/views/games/category-listing.blade.php
@@ -1,26 +1,14 @@
-<?php 
-$howToPlaySteps = App\ViewHelper::getCategoryCustomFieldContents(
-        $category->id, 
-        App\CustomFieldNames::HOW_TO_PLAY_STEPS
-    ); 
+<?php
+$howToPlaySteps = App\ViewHelper::getCategoryCustomFieldContents($category->id, App\CustomFieldNames::HOW_TO_PLAY_STEPS);
 ?>
-<?php 
-    $heroDescription = App\ViewHelper::getCategoryCustomFieldContents(
-        $category->id, 
-        App\CustomFieldNames::HERO_DESCRIPTION
-    ); 
+<?php
+$heroDescription = App\ViewHelper::getCategoryCustomFieldContents($category->id, App\CustomFieldNames::HERO_DESCRIPTION);
 ?>
-<?php 
-    $howToPlayShortDescription = App\ViewHelper::getCategoryCustomFieldContents(
-        $category->id, 
-        App\CustomFieldNames::HOW_TO_PLAY_SHORT_DESCRIPTION
-    ); 
+<?php
+$howToPlayShortDescription = App\ViewHelper::getCategoryCustomFieldContents($category->id, App\CustomFieldNames::HOW_TO_PLAY_SHORT_DESCRIPTION);
 ?>
-<?php 
-    $quote = App\ViewHelper::getCategoryCustomFieldContents(
-        $category->id, 
-        App\CustomFieldNames::GAME_QUOTE
-    ); 
+<?php
+$quote = App\ViewHelper::getCategoryCustomFieldContents($category->id, App\CustomFieldNames::GAME_QUOTE);
 ?>
 
 @extends('layouts.app')
@@ -31,82 +19,82 @@ $howToPlaySteps = App\ViewHelper::getCategoryCustomFieldContents(
 @section('description', strip_tags($heroDescription))
 <?php endif; ?>
 
-@section('page-class', 'category theme-'.$category->slug)
-@section('hero-class', 'hero-'. $category->slug)
+@section('page-class', 'category theme-' . $category->slug)
+@section('hero-class', 'hero-' . $category->slug)
 
 @section('hero-video')
-<?php new \App\Http\CustomView\Components\VideoPlayer($heroVideo); ?>
+    <?php new \App\Http\CustomView\Components\VideoPlayer($heroVideo); ?>
 @endsection
 
 @section('hero')
-<div class="content center">
-    <div class="title">
-        <img src="{{ \App\ViewHelper::getGameLogoPathByName($category->slug) }}" alt="Game logo" />
+    <div class="content center">
+        <div class="title">
+            <img src="{{ \App\ViewHelper::getGameLogoPathByName($category->slug) }}" alt="Game logo" />
+        </div>
+        <h1 class="small-h1">
+            <?php if($heroDescription): ?>
+            <?php print strip_tags($heroDescription, '<br>'); ?>
+            <?php endif; ?>
+        </h1>
+        <div class="buttons">
+            <a href="{{ $category->slug }}/how-to-play" class="btn btn-primary" title="How to play {{ $category->title }}">How to play</a>
+            <a href="#streams" class="btn btn-primary" title="{{ $category->title }} Streams">Streams</a>
+            <a href="#news" class="btn btn-primary" title="{{ $category->title }} News">News</a>
+        </div>
     </div>
-    <h1 class="small-h1">
-    <?php if($heroDescription): ?>
-        <?php print strip_tags($heroDescription, "<br>"); ?>
-    <?php endif; ?>
-    </h1>
-    <div class="buttons">
-        <a href="{{ $category->slug}}/how-to-play" class="btn btn-primary" title="How to play {{ $category->title }}">How to play</a>
-        <a href="#streams" class="btn btn-primary" title="{{ $category->title }} Streams">Streams</a>
-        <a href="#news" class="btn btn-primary" title="{{ $category->title }} News">News</a>
-    </div>
-</div>
 @endsection
 
 @section('content')
-<section id="buy" class="section section-grey section-buy">
-    <div class="main-content center">
-        <div class="feature-box">
-            <div class="feature-text">
-                <h1 class="section-title"><span class="light">How to play</span> <br /> {{ $category->title }}</h1>
-                <p class="section-description">
-                    <?php if($howToPlayShortDescription): ?>
-                        <?php print strip_tags($howToPlayShortDescription, "<br>"); ?>
-                    <?php endif; ?>
-                </p>
-                <div class="buttons">
-                    <a class="btn btn-primary" title="How to play" href="/{{ $category->slug}}/how-to-play">How to play guide</a>
+    <section id="buy" class="section section-grey section-buy">
+        <div class="main-content center">
+            <div class="feature-box">
+                <div class="feature-text">
+                    <h1 class="section-title"><span class="light">How to play</span> <br /> {{ $category->title }}</h1>
+                    <p class="section-description">
+                        <?php if($howToPlayShortDescription): ?>
+                        <?php print strip_tags($howToPlayShortDescription, '<br>'); ?>
+                        <?php endif; ?>
+                    </p>
+                    <div class="buttons">
+                        <a class="btn btn-primary" title="How to play" href="/{{ $category->slug }}/how-to-play">How to play guide</a>
+                    </div>
                 </div>
-            </div>
 
-            <div class="feature-banner">
-                <img src="{{ \App\ViewHelper::getFeatureBannerByGameSlug($category->slug) }}" alt="Game logo" />
-                <div class="quote">
-                    <?php if($quote): ?>
-                        <?php print strip_tags($quote, "<p>,<br>"); ?>
-                    <?php endif; ?>
+                <div class="feature-banner">
+                    <img src="{{ \App\ViewHelper::getFeatureBannerByGameSlug($category->slug) }}" alt="Game logo" />
+                    <div class="quote">
+                        <?php if($quote): ?>
+                        <?php print strip_tags($quote, '<p>,<br>'); ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section id="streams" class="section section-black stream-listings">
-    <div class="main-content">
-        <?php if (count($streams) == 0): ?>
+    <section id="streams" class="section section-black stream-listings">
+        <div class="main-content">
+            <?php if (count($streams) == 0): ?>
             <h2 class="section-title">Latest {{ $category->title }} videos</h2>
             <p>
                 Discover the latest {{ $category->title }} video content on Twitch
             </p>
             <?php new App\Http\CustomView\Components\TwitchVideoListing($videos); ?>
-        <?php else: ?>
+            <?php else: ?>
             <h2 class="section-title">{{ $category->title }} streamers</h2>
             <?php new App\Http\CustomView\Components\TwitchListing($streams); ?>
-        <?php endif; ?>
-    </div>
-</section>
+            <?php endif; ?>
+        </div>
+    </section>
 
-<section id="news" class="section section-grey news-listings">
-    <div class="main-content">
-        <h2 class="section-title">{{ $category->title }} News</h2>
-        <p class="section-description">
-            Discover the latest {{ $category->title }} news from around the C&C community. Including patches, maps, mods and much more!
-        </p>
-        <?php new App\Http\CustomView\Components\NewsListing($news); ?>
-    </div>
-</section>
+    <section id="news" class="section section-grey news-listings">
+        <div class="main-content">
+            <h2 class="section-title">{{ $category->title }} News</h2>
+            <p class="section-description">
+                Discover the latest {{ $category->title }} news from around the C&C community. Including patches, maps, mods and much more!
+            </p>
+            <?php new App\Http\CustomView\Components\NewsListing($news); ?>
+        </div>
+    </section>
 
 @endsection
--- src/resources/views/games/detail-how-to-play.blade.php
+++ src/resources/views/games/detail-how-to-play.blade.php
@@ -2,65 +2,67 @@
 
 @section('title', $page->title)
 @section('description', $page->description)
-@section('page-class', 'category theme-'.$slugCategory)
+@section('page-class', 'category theme-' . $slugCategory)
 @section('page-class', 'game-detail')
 
 @section('hero-video')
-<?php new \App\Http\CustomView\Components\VideoPlayer($heroVideo); ?>
+    <?php new \App\Http\CustomView\Components\VideoPlayer($heroVideo); ?>
 @endsection
 
 @section('hero')
-<div class="content center">
-    <div class="title">
-        <img src="{{ \App\ViewHelper::getGameLogoPathByName($slugCategory) }}" alt="Game logo" />
-    </div>
+    <div class="content center">
+        <div class="title">
+            <img src="{{ \App\ViewHelper::getGameLogoPathByName($slugCategory) }}" alt="Game logo" />
+        </div>
 
-    <h1 class="small-h1">
-        <?php print strip_tags($page->description, "<br>"); ?>
-    </h1>
-    <div class="buttons">
-        <a href="{{ $page->category()->url() }}" class="btn btn-primary" title="Go to {{ $page->category()->title }}">
-            Go back to {{ $page->category()->title }}
-        </a>
+        <h1 class="small-h1">
+            <?php print strip_tags($page->description, '<br>'); ?>
+        </h1>
+        <div class="buttons">
+            <a href="{{ $page->category()->url() }}" class="btn btn-primary" title="Go to {{ $page->category()->title }}">
+                Go back to {{ $page->category()->title }}
+            </a>
+            <a class="btn btn-primary" title="Buy on EA Origin" rel="nofollow" href="{{ route('pages.tuc') }}">
+                Where to Buy {{ $page->category()->title }}
+            </a>
+        </div>
     </div>
-</div>
 @endsection
 
 @section('content')
-<?php $howToPlaySteps = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_STEPS); ?>
-<?php if($howToPlaySteps): ?>
-<section class="section how-to-play-steps">
-    <div class="main-content">
-        <div class="page-content">
-            <?php print $howToPlaySteps; ?>
+    <?php $howToPlaySteps = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_STEPS); ?>
+    <?php if($howToPlaySteps): ?>
+    <section class="section how-to-play-steps">
+        <div class="main-content">
+            <div class="page-content">
+                <?php print $howToPlaySteps; ?>
+            </div>
         </div>
-    </div>
-</section>
-<?php endif; ?>
+    </section>
+    <?php endif; ?>
 
-<?php $howToPlayVideo = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_VIDEO); ?>
-<?php if($howToPlayVideo): ?>
-<section class="section section-grey video-tutorial">
-    <div class="main-content">
-        <div class="page-content center">
-            <h2>YouTube how to play steps</h2>
-            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/<?php print strip_tags($howToPlayVideo); ?>" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-            </iframe>
+    <?php $howToPlayVideo = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_VIDEO); ?>
+    <?php if($howToPlayVideo): ?>
+    <section class="section section-grey video-tutorial">
+        <div class="main-content">
+            <div class="page-content center">
+                <h2>YouTube how to play steps</h2>
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/<?php print strip_tags($howToPlayVideo); ?>" frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+            </div>
         </div>
-    </div>
-</section>
-<?php endif; ?>
+    </section>
+    <?php endif; ?>
 
-<?php $howToPlayHelp = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_HELP); ?>
-<?php if($howToPlayHelp): ?>
-<section class="section how-to-play-help">
-    <div class="main-content">
-        <div class="page-content">
-            <?php print $howToPlayHelp; ?>
+    <?php $howToPlayHelp = App\ViewHelper::getCustomFieldContents($page->id, App\CustomFieldNames::HOW_TO_PLAY_HELP); ?>
+    <?php if($howToPlayHelp): ?>
+    <section class="section how-to-play-help">
+        <div class="main-content">
+            <div class="page-content">
+                <?php print $howToPlayHelp; ?>
+            </div>
         </div>
-    </div>
-</section>
-<?php endif; ?>
+    </section>
+    <?php endif; ?>
 @endsection
