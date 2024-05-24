f41200ef7126b58cf8b40c8efac9e2f0e1fc02a4
Grant Bartlett <hello@grant-bartlett.com>
2024-05-24 16:21:45 +0100

    Admin tidy up

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container-fluid">
-    <div class="page-title">
-        <h3 class="mt-4">Dashboard</h3>
-    </div>
-
-    <div class="row">
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
-                <div class="card-body">News</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <a class="small text-white stretched-link" href="/admin/news">Manage News</a>
-                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
+    <div class="container-fluid">
+        <div class="page-title">
+            <h3 class="mt-4">Dashboard</h3>
         </div>
 
-        <?php if (auth()->user()->role == \App\User::ROLE_ADMIN) : ?>
+        <div class="row">
+            <div class="col-xl-3 col-md-6">
+                <div class="card bg-primary text-white mb-4">
+                    <div class="card-body">News</div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        <a class="small text-white stretched-link" href="/admin/news">Manage News</a>
+                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                    </div>
+                </div>
+            </div>
+
+            <?php if (auth()->user()->role == \App\User::ROLE_ADMIN) : ?>
             <div class="col-xl-3 col-md-6">
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body">Pages</div>
@@ -38,17 +38,8 @@
                 </div>
             </div>
 
-            <div class="col-xl-3 col-md-6">
-                <div class="card bg-primary text-white mb-4">
-                    <div class="card-body">Leaderboard</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-white stretched-link" href="/admin/leaderboard">Manage Leaderboard Season</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                    </div>
-                </div>
-            </div>
-        <?php endif; ?>
+            <?php endif; ?>
 
+        </div>
     </div>
-</div>
 @endsection
