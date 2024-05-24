377a81d79e5e0e390725698d1ec0bf35f32ae981
Grant Bartlett <hello@grant-bartlett.com>
2024-05-24 16:16:23 +0100

    Mobile menu fix

--- src/resources/views/layouts/navigation/mobile-menu.blade.php
+++ src/resources/views/layouts/navigation/mobile-menu.blade.php
@@ -47,20 +47,6 @@
     </div>
 
     <div class="category">
-        <div class="title"><a href="/command-and-conquer-remastered"title="Remastered Collection">C&amp;C Remastered</a></div>
-        <a href="/command-and-conquer-remastered#buy" title="Buy the Remastered Collection" class="nav-link">Where to buy</a>
-        <a href="/command-and-conquer-remastered#news" title="C&C News" class="nav-link">News</a>
-        <a href="/command-and-conquer-remastered#streams" title="C&C Streams" class="nav-link">Streams</a>
-        <a href="/command-and-conquer-remastered/workshop-mods" title="C&C Mods" class="nav-link">Steam Workshop</a>
-        <a href="/command-and-conquer-remastered#help" title="Help & Support" class="nav-link">Help &amp; Support</a>
-        <div class="title"><a href="/command-and-conquer-remastered/leaderboard"title="C&C Remastered Leaderboard">C&amp;C Remastered
-                Leaderboard</a></div>
-        <a href="/command-and-conquer-remastered/leaderboard/tiberian-dawn" title="Tiberian Dawn Leaderboard" class="nav-link">Tiberian Dawn</a>
-        <a href="/command-and-conquer-remastered/leaderboard/red-alert" title="Tiberian Dawn Leaderboard" class="nav-link">Red Alert</a>
-    </div>
-
-
-    <div class="category">
         <div class="title">Other</div>
         <a href="/cnc-streamers" title="Watch C&C Livestreams" class="nav-link nav-creators-count">Watch <span class="notification pop-in">LIVE</span></a>
         <a href="/stats" title="C&C Online Stats" class="nav-link">Online <span class="notification js-total-online"></span></a>
@@ -69,8 +55,7 @@
     <div class="social-links">
         <a href="https://www.facebook.com/groups/commandandconquer" title="C&C Facebook Group" rel="nofollow noreferrer" target="_blank"><i
                 class="icon-facebook"></i></a>
-        <a href="https://twitter.com/cnccomofficial" title="C&C Community Twitter" rel="nofollow noreferrer" target="_blank"><i
-                class="icon-twitter"></i></a>
+        <a href="https://twitter.com/cnccomofficial" title="C&C Community Twitter" rel="nofollow noreferrer" target="_blank"><i class="icon-twitter"></i></a>
         <a href="https://discord.gg/zktcZQY" title="C&C Discord" rel="nofollow noreferrer" target="_blank"><i class="icon-discord"></i></a>
         <a href="https://store.steampowered.com/franchise/CandC" title="C&C Steam" rel="nofollow noreferrer" target="_blank"><i class="icon-steam"></i></a>
         <a href="https://www.reddit.com/r/commandandconquer" title="C&C Reddit" rel="nofollow noreferrer" target="_blank"><i class="icon-reddit"></i></a>
