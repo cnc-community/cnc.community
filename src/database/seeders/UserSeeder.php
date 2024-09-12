9860010ea1b20d29152a8d6d2ab85cfbd00cb5f4
Daniel "Danku" McMahon <daniel40392@gmail.com>
2024-09-12 09:33:37 +0100

    Updating readme & matching UserSeeder classname (#55)

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\User;
 
-class UserSeed extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
